e12131624f5e95dbf4ae3eaa3007c124445d8d57
shanjb0221 <2533296339@qq.com>
2021-09-12 01:31:48 +0800

    feat: more channels

--- app/src/main/java/com/example/myapplication/constants/Constants.java
+++ app/src/main/java/com/example/myapplication/constants/Constants.java
@@ -8,17 +8,22 @@ import java.util.List;
 public class Constants {
     public final static List<ChannelEntity> allChannels = Arrays.asList(
             new ChannelEntity("全部", true),
-            new ChannelEntity("娱乐"),
-            new ChannelEntity("军事"),
-            new ChannelEntity("教育"),
-            new ChannelEntity("文化"),
-            new ChannelEntity("健康"),
-            new ChannelEntity("财经"),
             new ChannelEntity("体育"),
-            new ChannelEntity("汽车"),
+            new ChannelEntity("其他"),
             new ChannelEntity("科技"),
-            new ChannelEntity("社会"));
+            new ChannelEntity("汽车"),
+            new ChannelEntity("母婴"),
+            new ChannelEntity("新闻"),
+            new ChannelEntity("娱乐"),
+            new ChannelEntity("其它"),
+            new ChannelEntity("文化"),
+            new ChannelEntity("财经"),
+            new ChannelEntity("军事"),
+            new ChannelEntity("健康"),
+            new ChannelEntity("社会"),
+            new ChannelEntity("教育"),
+            new ChannelEntity("国内"));
 
-    public static String baseURL = "https://api2.newsminer.net/";
-    public static int pageSize = 25;
+    public final static String baseURL = "https://api2.newsminer.net/";
+    public final static int pageSize = 25;
 }
